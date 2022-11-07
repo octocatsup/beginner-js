fe8b417048bf12bd5139838da78db297d62970a1
octocatsup <75054337+octocatsup@users.noreply.github.com>
2022-11-07 03:46:33 -0600

    Initial working calculateBill

---
+++ playground/custom-functions/cf.js
@@ -0,0 +1,10 @@
+// Function definition
+function calculateBill() {
+  // This is the function body
+  let total = 100 * 1.0825;
+  return total;
+}
+
+// Function call or "Run"
+const billTotal = calculateBill();
+console.log(`Your total is $${billTotal}`);
