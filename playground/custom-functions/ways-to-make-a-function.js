7b08008026118a5a5a285c6d91006076310c2ad9
octocatsup <75054337+octocatsup@users.noreply.github.com>
2022-11-15 02:03:32 -0600

    In ways-to-make-a-function.js, add function types

---
+++ playground/custom-functions/ways-to-make-a-function.js
@@ -0,0 +1,26 @@
+// This is a function
+// function doctorize(firstName) {
+//   return `Dr. ${firstName}`;
+// }
+
+// This is an anonymous function
+// function (firstName) {
+//   return `Dr. ${firstName}`;
+// }
+
+// This a function expresssion
+// const doctorize = function(firstName) {
+//   return `Dr. ${firstName}`;
+// };
+
+// A normal function is hoisted, i.e., can run a function
+// before it is called
+// console.log(doctorizeHoisted("Peter"))
+
+// function doctorizeHoisted(firstName) {
+//   return `Dr. ${firstName}`;
+// };
+
+// Arrow function with one parameter, = (inches)
+// You can remove the parenthesis, = inches
+const inchToCM = (inches) => inches * 2.54;
